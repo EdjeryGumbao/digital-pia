2a6d7c4d98f95312890501bee926bfd1e948f929
edjerygumbao <egcgumbao@usep.edu.ph>
2023-07-02 16:14:32 +0800

    minor adjustments

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,4 +8,6 @@
     <a href="{{ url('/system_description') }}" class="btn btn-primary">Start Privacy Impact Assessment</a><br><br>
 
     <a href="{{ url('/process') }}" class="btn btn-primary">Start Privacy Impact Assessment v2</a>
+
+    
 @stop
